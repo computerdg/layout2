8abc0aa2a104d3d45166198eff03b54f23b991ad
broot <eorms3834@naver.com>
2024-06-10 20:49:28 +0900

    Add UI

--- app/src/main/java/com/example/geofencetest/complete_todayWorkout.java
+++ app/src/main/java/com/example/geofencetest/complete_todayWorkout.java
@@ -1,18 +1,29 @@
 package com.example.geofencetest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class complete_todayWorkout extends AppCompatActivity {
+
+    private Button completeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_today_workout);
+
+        completeButton = findViewById(R.id.completeButton);
+
+        completeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

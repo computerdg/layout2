ab3b850ccc1ce3c7573dfcd3bdfba7c8f2bdde00
ilyount02 <qkddlfdud02@gmail.com>
2024-06-24 15:26:19 +0900

    일영

--- app/src/main/java/com/example/geofencetest/challList_proto.java
+++ app/src/main/java/com/example/geofencetest/challList_proto.java
@@ -19,7 +19,16 @@ public class challList_proto extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chall_list_proto);
 
+        Button cl1 = (Button) findViewById(R.id.cl1);
         Button btn_makeChall = (Button) findViewById(R.id.btn_makeChall);
+
+        cl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ChallengeViewActivity.class);
+                startActivity(intent);
+            }
+        });
 
         btn_makeChall.setOnClickListener(new View.OnClickListener() {
             @Override

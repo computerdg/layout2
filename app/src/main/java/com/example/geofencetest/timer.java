8bd5c0cd92cd10d5729276b9eac725a6357a1507
ilyount02 <qkddlfdud02@gmail.com>
2024-06-25 17:46:24 +0900

    타이머

--- app/src/main/java/com/example/geofencetest/timer.java
+++ app/src/main/java/com/example/geofencetest/timer.java
@@ -1,6 +1,9 @@
 package com.example.geofencetest;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class timer extends AppCompatActivity {
 
+    private TextView timerText;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_timer);
 
+        timerText = findViewById(R.id.timer_text);
+        progressBar = findViewById(R.id.progress_bar);
+
+        startTimer();
+    }
+
+    private void startTimer() {
+        new CountDownTimer(60000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                int secondsRemaining = (int) (millisUntilFinished / 1000);
+                timerText.setText(secondsRemaining + "s");
+                progressBar.setProgress(60 - secondsRemaining);
+            }
+
+            public void onFinish() {
+                finish();
+            }
+        }.start();
     }
 }
